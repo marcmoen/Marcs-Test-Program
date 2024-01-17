d8fd2f4805d73477dbf69488cac9f50c1e3de079
admin <admin@DESKTOP-ATIAN6G>
2024-01-17 20:16:57 +0100

    Projektdateien hinzufügen.

---
+++ Marcs-Test-Program/Form1.Designer.vb
@@ -0,0 +1,74 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        MyButton = New Button()
+        TextBox1 = New TextBox()
+        Button1 = New Button()
+        SuspendLayout()
+        ' 
+        ' MyButton
+        ' 
+        MyButton.Location = New Point(586, 206)
+        MyButton.Name = "MyButton"
+        MyButton.Size = New Size(75, 23)
+        MyButton.TabIndex = 0
+        MyButton.Text = "Set Text"
+        MyButton.UseVisualStyleBackColor = True
+        ' 
+        ' TextBox1
+        ' 
+        TextBox1.Location = New Point(129, 72)
+        TextBox1.Name = "TextBox1"
+        TextBox1.Size = New Size(319, 23)
+        TextBox1.TabIndex = 1
+        ' 
+        ' Button1
+        ' 
+        Button1.Location = New Point(504, 280)
+        Button1.Name = "Button1"
+        Button1.Size = New Size(133, 40)
+        Button1.TabIndex = 2
+        Button1.Text = "Clear"
+        Button1.UseMnemonic = False
+        Button1.UseVisualStyleBackColor = True
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(7F, 15F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(800, 450)
+        Controls.Add(Button1)
+        Controls.Add(TextBox1)
+        Controls.Add(MyButton)
+        Name = "Form1"
+        Text = "Form1"
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents MyButton As Button
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents Button1 As Button
+
+End Class
---
+++ Marcs-Test-Program/Form1.vb
@@ -0,0 +1,13 @@
+﻿Public Class Form1
+    Private Sub MyButton_Click(sender As Object, e As EventArgs) Handles MyButton.Click
+        Dim Text As String
+        Text = "Blablabla"
+
+        TextBox1.AppendText(Text)
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        TextBox1.Clear()
+
+    End Sub
+End Class
